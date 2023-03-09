ee97f24858d1a6b9495ac766e8dac23c33d7170c
Fabio Calo <69361126+fcalod@users.noreply.github.com>
2023-03-09 12:11:36 -0600

    Add game state clauses

--- Proyecto Domino/Domino.pl
+++ Proyecto Domino/Domino.pl
@@ -1,38 +1,46 @@
 
-% Guarda qué fichas quedan
-% TODO: Cómo manejamos el orden de las entradas?
-rem_pieces( (0, 0) ).
-rem_pieces( (0, 1) ).
-rem_pieces( (0, 2) ).
-rem_pieces( (0, 3) ).
-rem_pieces( (0, 4) ).
-rem_pieces( (0, 5) ).
-rem_pieces( (0, 6) ).
-rem_pieces( (1, 1) ).
-rem_pieces( (1, 2) ).
-rem_pieces( (1, 3) ).
-rem_pieces( (1, 4) ).
-rem_pieces( (1, 5) ).
-rem_pieces( (1, 6) ).
-rem_pieces( (2, 2) ).
-rem_pieces( (2, 3) ).
-rem_pieces( (2, 4) ).
-rem_pieces( (2, 5) ).
-rem_pieces( (2, 6) ).
-rem_pieces( (3, 3) ).
-rem_pieces( (3, 4) ).
-rem_pieces( (3, 5) ).
-rem_pieces( (3, 6) ).
-rem_pieces( (4, 4) ).
-rem_pieces( (4, 5) ).
-rem_pieces( (4, 6) ).
-rem_pieces( (5, 5) ).
-rem_pieces( (5, 6) ).
-rem_pieces( (6, 6) ).
+% main inicializa el estado del juego
+% game_state guarda todo lo que se conoce acerca del estado actual: fichas que no se han jugado, número de fichas por jugador, 
+% número de fichas en el pozo, fichas que tenemos nosotros y las dos fichas abiertas en el tablero
+main:-
+  % Guarda qué fichas quedan
+  % TODO: Cómo manejamos el orden de las entradas?
+  game_state(rem_pieces, [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 3), (3, 4), (3, 5), (3, 6), (4, 4), (4, 5), (4, 6), (5, 5), (5, 6), (6, 6)]),
+  game_state(num_player_pcs, 7),
+  game_state(num_op_pieces, 7),
+  game_state(player_pieces, []),
+  game_state(open_pieces, [])
+  .
 
-% Guarda los valores disponibles de las dos fichas junto a las que puede colocarse una nueva
-% Inicia en un estado inválido, antes de que se coloquen fichas
-game_state(-1, -1).
+% Lee y guarda las fichas que tiene el jugador
+read_ini_player_hand():-
+  write("Ingresa cada ficha con el formato "(N1, N2)" y oprime enter"),
+  read(Piece),
+  read_ini_player_hand([Piece])
+  .
+read_ini_player_hand([Prev_piece | _]):-
+  read(Piece),
+  read_ini_player_hand([Piece, Prev_piece | _])
+  .
+
+
+
+% Se asegura que las fichas que se ingresen tengan el número menor a la izquierda
+order_input([N1, N2], Out):-
+  .
+order_input([N1, N2], Out):-
+  ( N1 > N2 ->
+    % reordena
+    order_input([N2, N1], Out);
+    % deja el orden igual
+    order_input([N1, N2], Out)
+  ).
+
+% Coloca la primera ficha
+place_first(Ficha):-
+  game_state
+  .
+  
 
 % TODO: ordenar una ficha de menor entrada a mayor antes de utilizarla / quitarla de la mesa
 % TODO: hacer que el sistema confirme la entrada o permita deshacer la jugada, en caso de que nos equivoquemos al ingresarla
@@ -40,6 +48,9 @@ game_state(-1, -1).
 place_piece([N1, N2], Side):-
   retract(rem_pieces( (N1, N2) )), % quita la ficha del pool disponible
   .
+
+% Métodos de interacción con el usuario
+create_
 
 
 
