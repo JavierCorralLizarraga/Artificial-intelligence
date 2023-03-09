d46dc1adadeb79ae77f9e8cc32f2952c9276dd45
Fabio Calo <69361126+fcalod@users.noreply.github.com>
2023-03-09 12:59:21 -0600

    Add order_input

--- Proyecto Domino/Domino.pl
+++ Proyecto Domino/Domino.pl
@@ -6,7 +6,7 @@ main:-
   % Guarda qué fichas quedan
   % TODO: Cómo manejamos el orden de las entradas?
   game_state(rem_pieces, [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 3), (3, 4), (3, 5), (3, 6), (4, 4), (4, 5), (4, 6), (5, 5), (5, 6), (6, 6)]),
-  game_state(num_player_pcs, 7),
+  game_state(num_player_pieces, 7),
   game_state(num_op_pieces, 7),
   game_state(player_pieces, []),
   game_state(open_pieces, [])
@@ -16,24 +16,22 @@ main:-
 read_ini_player_hand():-
   write("Ingresa cada ficha con el formato "(N1, N2)" y oprime enter"),
   read(Piece),
-  read_ini_player_hand([Piece])
+  order_input(Piece, Ordered_piece),
+  read_ini_player_hand([Ordered_piece])
   .
 read_ini_player_hand([Prev_piece | _]):-
   read(Piece),
   read_ini_player_hand([Piece, Prev_piece | _])
   .
 
-
-
 % Se asegura que las fichas que se ingresen tengan el número menor a la izquierda
-order_input([N1, N2], Out):-
-  .
+order_input_aux(Out, Out). % Iguala Out a la entrada
 order_input([N1, N2], Out):-
   ( N1 > N2 ->
     % reordena
-    order_input([N2, N1], Out);
+    order_input_aux([N2, N1], Out);
     % deja el orden igual
-    order_input([N1, N2], Out)
+    order_input_aux([N1, N2], Out)
   ).
 
 % Coloca la primera ficha
