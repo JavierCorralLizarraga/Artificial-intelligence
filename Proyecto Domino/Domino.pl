89e414413dcf92ff3852944a7f2a787008fe1245
Fabio Calo <69361126+fcalod@users.noreply.github.com>
2023-03-14 15:29:40 -0600

    Fix spacing

--- Proyecto Domino/Domino.pl
+++ Proyecto Domino/Domino.pl
@@ -11,9 +11,9 @@
 main:-
   % Guarda qué fichas no se han jugado
   assert(game_state(rem_tiles, [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 1), 
-  															(1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 2), (2, 3), (2, 4), 
-  															(2, 5), (2, 6), (3, 3), (3, 4), (3, 5), (3, 6), (4, 4), (4, 5), 
-  															(4, 6), (5, 5), (5, 6), (6, 6)])),
+  				(1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 2), (2, 3), (2, 4), 
+  				(2, 5), (2, 6), (3, 3), (3, 4), (3, 5), (3, 6), (4, 4), (4, 5), 
+  				(4, 6), (5, 5), (5, 6), (6, 6)])),
   assert(game_state(num_player_tiles, 0)), % inicia en 0 para contarlas después
   assert(game_state(num_oppo_tiles, 7)),
   assert(game_state(player_hand, [])),
